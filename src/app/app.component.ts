05c286313c583896eef5b67bd71d0627235997a0
msantosbl <msantosbl@uoc.edu>
2024-10-09 20:24:20 +0200

    Navbar ultra cutre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import	{JugadorCardComponent} from './jugador-card/jugador-card.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JugadorCardComponent],
+  imports: [RouterOutlet, JugadorCardComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
