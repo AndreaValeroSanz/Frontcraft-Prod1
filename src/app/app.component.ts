f1a2a930006d93a2fd74c17baf1883b76e49f84f
AndreaValeroSanz <avalerosanz@uoc.edu>
2024-10-09 19:53:12 +0200

    Primera iteracion playerCard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import	{JugadorCardComponent} from './jugador-card/jugador-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JugadorCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/jugador-card/jugador-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jugador-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './jugador-card.component.html',
+  styleUrl: './jugador-card.component.css'
+})
+export class JugadorCardComponent {
+
+}
