c564e90089102c95e1c192c2f6db6438831d1b8b
msantosbl <msantosbl@uoc.edu>
2024-10-10 11:51:57 +0200

    Probando el router para la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {JugadorCardComponent} from './jugador-card/jugador-card.component';
+import {AboutComponent} from './about/about.component';
+import {StadisticsComponent} from './stadistics/stadistics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: JugadorCardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'stadistics', component: StadisticsComponent }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
