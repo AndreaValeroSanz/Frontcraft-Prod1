864f082eeabf3d3bf58f68003fea9e2f43cc6a92
Hectorjnavas98 <hjimenez.navas@gmail.com>
2024-10-11 15:19:54 +0200

    Añadido Herocomponent a dashboard, incluida la lógica de scroll y diseño al inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { JugadorCardModule } from './jugador-card/jugador-card.module';
+import { HeroComponent } from './hero/hero.component'; // Importa el HeroComponent
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JugadorCardModule], // Update this line
+  imports: [RouterOutlet, JugadorCardModule, HeroComponent], // Agrega HeroComponent aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
